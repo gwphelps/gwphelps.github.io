e5f137555721a14c2645e8d244a144462ceaef23
Garet Phelps <garetphelps@gmail.com>
2020-07-20 21:33:08 -0500

    added hamburger menu for mobile

--- src/index.jsx
+++ src/index.jsx
@@ -5,29 +5,40 @@ import Projects from "./projects.jsx";
 import Education from "./education.jsx";
 import Contact from "./contact.jsx";
 import About from "./about.jsx";
+import NavBar from "./navbar/NavBar.jsx"
 
 class HomeComponent extends Component {
   constructor(props){
     super(props);
+    this.redirect = this.redirect.bind(this);
     this.state = {
       content: <Home/>
     };
-
+  }
+  redirect(route){
+    let state = "";
+    switch(route){
+      case "Home": state = <Home/>;break;
+      case "About": state = <About/>;break;
+      case "Education": state = <Education/>;break;
+      case "Projects": state = <Projects/>;break;
+      case "Contact": state = <Contact/>;break;
+    }
+    if(state != ""){
+      this.setState({content: state});
+    }
   }
   render(){
     return(
       <div>
-        <div id="header">
-          <div id="name">
-            <h1 className="clickable" onClick={() => this.setState({content:<Home/>})}>Garet Phelps</h1>
-          </div>
-          <ul id="nav">
-            <li className="nav-item clickable" onClick={() => this.setState({content: <About/>})}>About</li>
-            <li className="nav-item clickable" onClick={() => this.setState({content: <Education/>})}>Education</li>
-            <li className="nav-item clickable" onClick={() => this.setState({content: <Projects/>})}>Projects</li>
-            <li className="nav-item clickable" onClick={() => this.setState({content: <Contact/>})}>Contact</li>
-          </ul>
-        </div>
+        <NavBar
+          title="Garet Phelps"
+          pages={
+            ["About", "Education", "Projects", "Contact"]
+          }
+          disappearOnScroll={false}
+          redirect={(route) => this.redirect(route)}
+        />
         <div id="content">
           {this.state.content}
         </div>
